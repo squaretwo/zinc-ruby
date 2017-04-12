1b04fc72484f7e3afc1558f83a8a63682d611888
Ben Yee <ben@squaretwo.co>
2017-04-12 15:21:01 -0700

    update #test_order_create with the latest params

--- spec/test_content.rb
+++ spec/test_content.rb
@@ -1,33 +1,56 @@
 def test_order_create
   {
-      :mode => "dev",
-      :max_total => 6000,
-      :address => {
-          :name => "Tim Beaver",
-          :address_line1 => "77 Massachusetts Avenue",
-          :address_line2 => nil,
-          :zip_code => "02139",
-          :city => "Cambridge",
-          :state => "MA",
-          :country => "US"
-      },
-      :address_suggestion_choice => "suggested",
-      :products => [
-          {
-              :pid => "B003L1ZYYM",
-              :pid_type => "ASIN",
-              :qty => 1
-          },
-          {
-              :pid => "0833030477",
-              :pid_type => "ASIN",
-              :qty => 1
-          }
-      ],
-      :merchant => "amazon",
-      :gift_ship => true,
-      :gift_message => "Thanks for ordering from example.com!",
-      :shipping_method => "standard"
+    retailer: 'amazon',
+    is_gift: true,
+    shipping_method: 'free',
+    gift_message: 'Thanks for your order, Foo Baz. Enjoy!',
+    shipping_address: {
+      first_name: 'Foo',
+      last_name: 'Baz',
+      address_line1: '123 3rd Street',
+      address_line2: '',
+      zip_code: 95112,
+      city: 'San Jose',
+      state: 'CA',
+      country: 'US',
+      phone_number: '4085551212'
+   },
+   billing_address: {
+     first_name: 'Foo',
+     last_name: 'Baz',
+     address_line1: '1 Mission Street',
+     address_line2: '',
+     zip_code: '94105',
+     city: 'San Francisco',
+     state: 'CA',
+     country: 'US',
+     phone_number: '4155551212'
+  },
+  payment_method: {
+    name_on_card: ENV['AMAZON_CC_NAME'],
+    number: ENV['AMAZON_CC_NUM'],
+    security_code: ENV['AMAZON_CC_CVV2'],
+    expiration_month: ENV['AMAZON_CC_EXP_MO'],
+    expiration_year: ENV['AMAZON_CC_EXP_YR'],
+    use_gift: false
+  },
+  retailer_credentials: {
+    email: ENV['AMAZON_ASSOC_EMAIL'],
+    password: ENV['AMAZON_ASSOC_PASSWORD']
+  },
+  address_suggestion_choice: 'suggested',
+    products: [
+      {
+        product_id: 'B01M0RU6LY',
+        pid_type: 'ASIN',
+        quantity: 1,
+        seller_selection_criteria: {
+          prime: true,
+          handling_days_max: 6,
+          condition_in: ['New'],
+        }
+      }
+    ]
   }
 end
 
